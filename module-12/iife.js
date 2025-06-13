bfaaf79bb28021c272d5ab152a45bd9cf80cb880
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-13 17:15:18 +0600

    Learn : Node JS concept using IIFE

---
+++ module-12/iife.js
@@ -0,0 +1,10 @@
+
+((name) => {
+  console.log(`Learning - ${name}`);
+})("Node JS");
+
+// console.log(module);       // Module object
+// console.log(require);      // Function to require other modules
+// console.log(__dirname);    // Directory name of the current module
+// console.log(__filename);   // File name of the current module
+
