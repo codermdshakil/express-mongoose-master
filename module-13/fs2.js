6f13d88eac05ac328185f553ac5c01dd66d54ec7
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-14 01:49:44 +0600

    Learn : Asynchronously read and write file

---
+++ module-13/fs2.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+// asynchrounously read and write 
+
+// fs.readFile("./hello-word.txt", {encoding:'utf8'}, (err, data) => {
+//   if(err){
+//     console.log("Error Occured", err);
+//   }
+  
+//   fs.writeFile('./hello.txt', data, {encoding:'utf8'}, (err) => {
+//     if(err){
+//       console.log('Error Occured', err);
+//     }
+//     console.log("Data write successfully!");
+//   })
+ 
+// })
