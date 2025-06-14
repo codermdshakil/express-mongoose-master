9bbc8d87d7f556e951338e234e0929ec41463af8
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-14 15:57:40 +0600

    Learn: POST method

--- module-13/todo-app/server.js
+++ module-13/todo-app/server.js
@@ -1,13 +1,6 @@
 const http = require("http");
-const path = require('path');
-const fs = require('fs');
-
-const person = {
-  id: 1,
-  name: "John Doe",
-  age: 25,
-};
-
+const path = require("path");
+const fs = require("fs");
 
 // fs.readFile("./todo-app/db/todo.json", {encoding:"utf8"},(err, data) => {
 //   if(err){
@@ -17,27 +10,50 @@ const person = {
 //     console.log(data);
 //   }
 // })
- 
 
-const filePath = path.join(__dirname, "./todo-app/db/todo.json")
-
+const filePath = path.join(__dirname, "./db/todo.json");
 
 const server = http.createServer((req, res) => {
-
+  // get all todos
   if (req.url === "/todos" && req.method === "GET") {
-    
-    const data = fs.readFileSync('./todo-app/db/todo.json', {encoding:"utf8"});
+    const data = fs.readFileSync("./todo-app/db/todo.json", {
+      encoding: "utf8",
+    });
+
     // way - 1:  set header [recommonded]
     res.writeHead(200, {
-      "content-type":"application/json",
-      
-    })
-
+      "content-type": "application/json",
+    });
     res.end(data);
+  }
+  // Create a new todo
+  else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    let data = "";
 
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
 
-  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
-    res.end("Create a new Todo!");
+    req.on("end", () => {
+      const todo = JSON.parse(data);
+
+      // 1. Read existing todos
+      const allTodos = fs.readFileSync(filePath, { encoding: "utf8" });
+      const parseDatas = JSON.parse(allTodos);
+
+      // 2. Add new todo
+      parseDatas.push(todo);
+
+      // 3. ✅ Write updated todos to file (FIXED)
+      fs.writeFileSync(filePath, JSON.stringify(parseDatas), {
+        encoding: "utf8",
+      });
+
+      // 4. Response after writing
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Todo created successfully!" }));
+    });
+
   } else {
     res.end("Enter valid url and method");
   }
