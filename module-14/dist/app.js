f20ab774952bd901201b83ff9d91cd8c4a3e49b0
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-17 11:18:59 +0600

    Learn: express get todos using file system

--- module-14/dist/app.js
+++ module-14/dist/app.js
@@ -4,11 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express_1.default.json());
+app.get("/", (req, res) => {
+    console.log({ req, res });
+    res.send("Today I Learning Express JS with TypeScript !");
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.get("/todos", (req, res) => {
+    const filePath = "./db/todo.json";
+    const data = fs_1.default.readFileSync(filePath, { encoding: "utf8" });
+    res.send(data);
 });
+app.post("/todos/create-todo", (req, res) => {
+    res.send("Get all todos!");
+});
+exports.default = app;
+/**
+ *
+ * Basic file structure:
+ *
+ * Server.ts -> Handle server like starting, closing, error handleding only related to server
+ * app.js -> handle routing, middleware , route related error
+ * app folder -> app business logic handling like Create, Read, Update, Delete, Database related works
+ *
+ *
+ */
--- module-14/src/app.ts
+++ module-14/src/app.ts
@@ -1,32 +1,38 @@
-
 import express, { Application, Request, Response } from "express";
+import fs from "fs";
 
-const app : Application = express();
+const app: Application = express();
 
-
-app.get('/', (req :Request , res : Response ) => {
-  res.send('Hello World!')
-})
-
-app.get('/todos', (req :Request , res : Response ) => {
-  res.send('Get all todos!')
-})
-app.post('/todos/create-todo', (req :Request , res : Response ) => {
-  res.send('Get all todos!')
-})
+app.use(express.json());
 
 
+
+app.get("/", (req: Request, res: Response) => {
+  console.log({ req, res });
+  res.send("Today I Learning Express JS with TypeScript !");
+});
+
+app.get("/todos", (req: Request, res: Response) => {
+
+  const filePath = "./db/todo.json";
+  const data = fs.readFileSync(filePath, {encoding:"utf8"});
+  res.send(data);
+
+});
+
+app.post("/todos/create-todo", (req: Request, res: Response) => {
+  res.send("Get all todos!");
+});
 
 export default app;
 
-
 /**
- * 
+ *
  * Basic file structure:
- * 
+ *
  * Server.ts -> Handle server like starting, closing, error handleding only related to server
  * app.js -> handle routing, middleware , route related error
  * app folder -> app business logic handling like Create, Read, Update, Delete, Database related works
- * 
- * 
-*/
+ *
+ *
+ */
