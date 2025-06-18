bd2d87a8f785533d81345315da231f89e623da69
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-18 16:12:22 +0600

    Learn : Intregrate Mongodb with express application

--- practice-express-typescript/dist/server.js
+++ practice-express-typescript/dist/server.js
@@ -1,14 +1,35 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
+const { MongoClient, ServerApiVersion } = require('mongodb');
 let server;
 const port = 5000;
-const bootstrap = () => {
+const uri = "mongodb+srv://mongodb:mongodb@cluster0.qo2wzoe.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+const bootstrap = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield client.connect();
+    console.log("successfully connected to MongoDB!");
     server = app_1.default.listen(port, () => {
         console.log("Server is listening on port ", port);
     });
-};
+});
 bootstrap();
--- practice-express-typescript/src/server.ts
+++ practice-express-typescript/src/server.ts
@@ -1,10 +1,31 @@
 import app from "./app";
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 let server;
-
 const port = 5000;
 
-const bootstrap = () => {
+
+
+const uri = "mongodb+srv://mongodb:mongodb@cluster0.qo2wzoe.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+
+
+const bootstrap = async () => {
+
+  await client.connect();
+ 
+  console.log("successfully connected to MongoDB!");
+
   server = app.listen(port, () => {
     console.log("Server is listening on port ", port);
   })
