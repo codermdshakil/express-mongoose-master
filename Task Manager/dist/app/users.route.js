67f235d6d91e556b0b44a09fd19eda455867a0a0
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-07-21 00:10:21 +0600

    Update a single user

--- Task Manager/dist/app/users.route.js
+++ Task Manager/dist/app/users.route.js
@@ -17,7 +17,7 @@ const mongodb_1 = require("mongodb");
 const mongoDB_1 = require("../config/mongoDB");
 const usersRouter = express_1.default.Router();
 // get all users
-usersRouter.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+usersRouter.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const db = yield mongoDB_1.client.db("users");
     const collection = yield db.collection("user");
     const cursor = collection.find({});
@@ -25,7 +25,7 @@ usersRouter.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* (
     res.json(data);
 }));
 // create a new user
-usersRouter.post('/create-user', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+usersRouter.post("/create-user", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { name, age, email, address } = req.body;
     const db = yield mongoDB_1.client.db("users");
     const collection = yield db.collection("user");
@@ -34,7 +34,7 @@ usersRouter.post('/create-user', (req, res) => __awaiter(void 0, void 0, void 0,
     res.json({ name, age, email, address });
 }));
 // get a single user
-usersRouter.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+usersRouter.get("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const id = req.params.id;
     const db = yield mongoDB_1.client.db("users");
     const collection = yield db.collection("user");
@@ -42,11 +42,21 @@ usersRouter.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function
     res.json(data);
 }));
 // delete a single user
-usersRouter.delete('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+usersRouter.delete("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const id = req.params.id;
     const db = yield mongoDB_1.client.db("users");
     const collection = yield db.collection("user");
     const data = yield collection.deleteOne({ _id: new mongodb_1.ObjectId(id) });
+    res.json(data);
+}));
+// update a user
+usersRouter.put("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const { name, age, email, address } = req.body;
+    const db = yield mongoDB_1.client.db("users");
+    const collection = yield db.collection("user");
+    const filter = { _id: new mongodb_1.ObjectId(id) };
+    const data = yield collection.updateOne(filter, { $set: { name, age, email, address } }, { upsert: true });
     res.json(data);
 }));
 exports.default = usersRouter;
--- Task Manager/src/app/users.route.ts
+++ Task Manager/src/app/users.route.ts
@@ -1,69 +1,71 @@
-
-import express, { Request, Response } from 'express';
-import { ObjectId } from 'mongodb';
-import { client } from '../config/mongoDB';
-
+import express, { Request, Response } from "express";
+import { ObjectId } from "mongodb";
+import { client } from "../config/mongoDB";
 
 const usersRouter = express.Router();
 
-
 // get all users
-usersRouter.get('/', async (req: Request, res: Response) => {
-
+usersRouter.get("/", async (req: Request, res: Response) => {
   const db = await client.db("users");
   const collection = await db.collection("user");
 
-  const cursor =  collection.find({});
+  const cursor = collection.find({});
   const data = await cursor.toArray();
 
   res.json(data);
-
 });
 
-
 // create a new user
-usersRouter.post('/create-user', async (req: Request, res: Response) => {
-
-  const {name, age, email, address} = req.body;
+usersRouter.post("/create-user", async (req: Request, res: Response) => {
+  const { name, age, email, address } = req.body;
 
   const db = await client.db("users");
   const collection = await db.collection("user");
-  collection.insertOne({name, age, email, address});
+  collection.insertOne({ name, age, email, address });
 
   console.log(`${name} added Successfully!`);
-  res.json({name, age, email, address})
-
+  res.json({ name, age, email, address });
 });
 
-
 // get a single user
-usersRouter.get('/:id', async (req: Request, res: Response) => {
-
+usersRouter.get("/:id", async (req: Request, res: Response) => {
   const id = req.params.id;
 
   const db = await client.db("users");
   const collection = await db.collection("user");
 
-  const data = await collection.findOne({_id: new ObjectId(id)});
+  const data = await collection.findOne({ _id: new ObjectId(id) });
 
-  res.json(data)
-  
+  res.json(data);
 });
 
 // delete a single user
-usersRouter.delete('/:id', async (req: Request, res: Response) => {
-
+usersRouter.delete("/:id", async (req: Request, res: Response) => {
   const id = req.params.id;
 
   const db = await client.db("users");
   const collection = await db.collection("user");
 
-  const data = await collection.deleteOne({_id: new ObjectId(id)});
+  const data = await collection.deleteOne({ _id: new ObjectId(id) });
 
-  res.json(data)
-  
+  res.json(data);
 });
 
+// update a user
+usersRouter.put("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
 
+  const { name, age, email, address } = req.body;
+
+  const db = await client.db("users");
+  const collection = await db.collection("user");
+
+  const filter = {_id: new ObjectId(id)};
+
+  const data = await collection.updateOne(filter, {$set: {name, age,  email, address}}, {upsert:true})
+  
+  res.json(data)
+
+});
 
 export default usersRouter;
