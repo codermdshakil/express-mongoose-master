1e1d678e68b6d21a7b4df7e2a4780e2f314468e5
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-07-22 15:10:17 +0600

    Get all notes

--- module-17/mongoose-note-app/src/app.ts
+++ module-17/mongoose-note-app/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import { model, Schema } from "mongoose";
 
 const app: Application = express();
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
@@ -16,42 +17,57 @@ const noteSchema = new Schema({
 // create note model
 const Note = model("Note", noteSchema);
 
+// get all notes
+
+app.get("/notes", async (req: Request, res: Response) => {
+  try {
+
+    const notes = await Note.find(); 
+
+    res.status(200).json(notes);
+
+  } catch (err) {
+
+    res.status(500).json({ error: "Failed to fetch notes" });
+
+  }
+});
+
+// create a note
 app.post("/create-note", async (req: Request, res: Response) => {
+  const { title, content } = req.body;
 
   const myNote = new Note({
-     title: "Mongoose",
-     content: "Learning Mongoose",
+    title,
+    content,
   });
 
   await myNote.save();
 
   res.status(201).json({
-    success:true,
-    message:`${myNote.title} note created successfully!`,
-    note:myNote
-  })
+    success: true,
+    message: `${myNote.title} note created successfully!`,
+    note: myNote,
+  });
 });
 
-
 // get a Single Note using Id
-app.get('/notes/:id', (req: Request, res: Response) => {
-
+app.get("/notes/:id", (req: Request, res: Response) => {
   const id = req.params.id;
 
   Note.findById(id)
-  .then(user => {
-    if (user) {
-      console.log('Found Note:', user);
-      res.json(user)
-    } else {
-      console.log('User not found.');
-    }
-  })
-  .catch(err => {
-    console.error('Error finding user:', err);
-  });
- 
-
-})
+    .then((user) => {
+      if (user) {
+        console.log("Found Note:", user);
+        res.json(user);
+      } else {
+        console.log("User not found.");
+      }
+    })
+    .catch((err) => {
+      console.error("Error finding user:", err);
+    });
+    
+});
 
 export default app;
