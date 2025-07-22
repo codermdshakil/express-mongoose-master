2e9fb9a4be4189a71de2721fd428e3c18bd18610
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-07-22 14:32:29 +0600

    Create: userSchema, Model and user

--- module-17/mongoose-note-app/src/app.ts
+++ module-17/mongoose-note-app/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 const app : Application = express();
 
@@ -7,6 +8,31 @@ app.get('/', (req:Request, res:Response) => {
 });
 
 
+// Steps
+// ---------
+// Schema
+// Model
+// User
 
+
+// User Schema or Structure
+const userSchema = new mongoose.Schema({
+  name:String,
+  age:Number,
+  isHired:Boolean
+});
+
+// User Model
+const User = mongoose.model("User", userSchema);
+
+
+// User
+const shakil = new User({name:"Shakil Ahmed", age:20, isHired:false});
+const noyon = new User({name:"Noyon Rahman", age:22, isHired:true});
+const nadim = new User({name:"Nadim", age:21, isHired:true});
+
+
+
+ 
 
 export default app;
