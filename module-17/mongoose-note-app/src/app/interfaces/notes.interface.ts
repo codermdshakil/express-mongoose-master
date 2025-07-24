25b267aa275c106e8db140e7de0cbb9da9ad996d
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-07-25 00:45:34 +0600

    Update: Notes Interface

--- module-17/mongoose-note-app/src/app/interfaces/notes.interface.ts
+++ module-17/mongoose-note-app/src/app/interfaces/notes.interface.ts
@@ -1,7 +1,7 @@
 export interface INotes {
   title: string;
   content: string;
-  category: string;
+  category: "Personal" | "Work" | "Study" | "Others";
   pinned: boolean;
   tags: {
     level: string;
