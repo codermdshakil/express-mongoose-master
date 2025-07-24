45c10437212ee14437148f61275e8f612c6481a1
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-07-25 00:26:12 +0600

    Learn: Get a single user

--- module-17/mongoose-note-app/src/app/controllers/user.controllers.ts
+++ module-17/mongoose-note-app/src/app/controllers/user.controllers.ts
@@ -3,20 +3,28 @@ import { User } from "../models/users.model";
 
 const userRoutes = express.Router();
 
-
 // post a User
-
-userRoutes.post("/create-user",  async (req: Request, res: Response) => {
-
+userRoutes.post("/create-user", async (req: Request, res: Response) => {
   const body = req.body;
 
   const user1 = await User.create(body);
 
   res.status(201).json({
-    message:"Successfully Created",
-    user:user1
-  })
+    message: "Successfully Created",
+    user: user1,
+  });
+});
 
+// get a single user
+userRoutes.get("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const findedUser = await User.findById(id);
+
+  res.status(201).json({
+    message: "Successfully get User",
+    user: findedUser,
+  });
 });
 
 export default userRoutes;
