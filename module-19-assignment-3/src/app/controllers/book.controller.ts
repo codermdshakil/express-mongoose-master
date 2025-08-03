5b8db06cb6b056323c1bb9417eb29849afe587bc
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-08-03 20:37:54 +0600

    Get book by id

--- module-19-assignment-3/src/app/controllers/book.controller.ts
+++ module-19-assignment-3/src/app/controllers/book.controller.ts
@@ -2,8 +2,10 @@ import express, { Request, Response } from "express";
 import z from "zod";
 import { Book } from "../models/book.model";
 
+// book router
 const bookRouter = express.Router();
 
+// book zod validation
 const CreateBookZodSchema = z.object({
   title: z.string(),
   author: z.string(),
@@ -14,7 +16,7 @@ const CreateBookZodSchema = z.object({
   available: z.boolean(),
 });
 
-// create a book
+//1. create a book
 bookRouter.post("/books", async (req: Request, res: Response) => {
   try {
     // validate data using zod
@@ -29,16 +31,40 @@ bookRouter.post("/books", async (req: Request, res: Response) => {
       message: "Book created successfully",
       data: data,
     });
-
-  } catch (error:any) {
-
+  } catch (error: any) {
     res.status(400).json({
       success: false,
       message: error.message,
       error,
     });
-    
   }
+});
+
+// 2. get all books
+
+// 3. get book by id
+bookRouter.get("/books/:bookId", async (req: Request, res: Response) => {
+  const bookId = req.params.bookId;
+
+  try {
+    const book = await Book.findById(bookId);
+
+    // response
+    res.json({
+      success: true,
+      message: "Book retrieved successfully",
+      data: book,
+    });
+
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+      error,
+    });
+  }
+
+  res.send(bookId);
 });
 
 export default bookRouter;
